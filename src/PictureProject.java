2bf0215c8fa975f1c76a6851b701b60f531c128a
Amara <amara_prather26@milton.edu>
2025-11-11 22:49:56 -0500

    Finished Picture

--- src/PictureProject.java
+++ src/PictureProject.java
@@ -1,12 +1,10 @@
 import java.util.ArrayList;
 
 public class PictureProject extends World {
-    // Night sky with stars (use & loops params for diff sized starts) animated??
-    // gradient night sky (maybe two way from the moon)
-
-    public int vred;
-    public int vgreen;
-    public int vblue;
+    //add varibles for red, green, blue, and x & y coordinates for
+    int vred;
+    int vgreen;
+    int vblue;
     int xcoord;
     int ycoord;
 
@@ -14,30 +12,19 @@ public class PictureProject extends World {
        // gradient background from nested for loop
         plane.pausetime = 0;
         plane.setPos(0,0);
-        //cover every x coord
+        //covers every x coord
         for (int x=0; x<1000; x=x+1) {
-            //covers every y coord
-            for (int y = 0; y < 800; y = y + 1) {
+            //covers every y coord for the upper half of the screen
+            for (int y = 0; y < 500; y = y + 1) {
                 plane.setPos(x,y);
-                plane.setPixelColor(y/5, y/4, 30+y/4);
-               /* if (y<200) {
-                    plane.setPixelColor(y / 5, y / 4, 30 + y /10);
-                }
-                else {
-                    plane.setPixelColor(y/5, y/4, 30+y/4);
-                }
-                int blue = 2^(y/101);
-                plane.setPixelColor(0,0, blue);
-                System.out.println (blue);
-                0,0,0
-                 190, 190, 255*/
+                plane.setPixelColor(y/5, y/4, 30+y/4); //y fractions make gradient
             }
         }
         //stars
-        int ransize;
-        int ranx;
-        int rany;
-        for (int x=0; x<30; x=x+1){
+        int ransize;//random size
+        int ranx;//random x position
+        int rany;//random y position
+        for (int x=0; x<30; x=x+1){//loops to make 30 stars, in 30 random sizes, at 30 random positions
             ransize = plane.random(20,40);
             ranx = plane.random(0, 1000);
             rany = plane.random(0, 450);
@@ -47,40 +34,31 @@ public class PictureProject extends World {
             else {
                 plane.setPos(ranx, rany);
             }
-            star(ransize);
+            star(ransize);//calls start method to make a star with a random side length
         }
-        //moon
-        plane.loadBackGround("PrincessCarolyn.jpeg");
-        //plane.showBackGround();
-        //change size
-
-
+        //add picture of the moon
+        plane.loadBackGround("moon.jpg");
+        //make picture smaller
         for (int x=0; x < 300; x=x+1) {
             for (int y = 0; y < 250; y = y + 1){
                 xcoord = x*10/3;
-                ycoord = y*67/25;
+                ycoord = y*80/25;
                 plane.teleport(xcoord, ycoord);
                 vred = plane.howMuchRed();
                 vblue = plane.howMuchBlue();
                 vgreen = plane.howMuchGreen();
                 plane.teleport(x,y);
-                plane.setPixelColor(vred, vgreen, vblue);
+                //ignores the black background on the moon photo-- only adds the picture where it's gray
+                if (vred>0 && vgreen>0 && vblue>0) {
+                    plane.setPixelColor(vred, vgreen, vblue);
+                }
             }
 
         }
-
-        //filter out black part
-
-        /*if (vred>10 && vblue>10 && vgreen>10){
-            plane.setpixel color (
-        }
-         */
-
-        /*plane.setPos(250,150);
-        plane.circle(50);*/
-
+        //calls land method to add green part
+        land();
     }
-
+    //method to make stars with a parameter for size/side length
     public void star(int size){
         plane.setColor(250, 250, 255);
         plane.isTrail = true;
@@ -92,7 +70,19 @@ public class PictureProject extends World {
 
     }
 
-    public void person (){
+    //method to make land--a green rectangle on the lower half of the screen
+    public void land () {
+        plane.setPos(0, 500);
+        //covers x direction
+        for (int x = 0; x < 1000; x = x + 1) {
+            //covers the y direction
+            for (int y = 500; y < 800; y = y + 1) {
+                //sets plain to green color
+                plane.teleport(x, y);
+                plane.setPixelColor(1, 74, 20);
+            }
 
+
+        }
     }
 }
